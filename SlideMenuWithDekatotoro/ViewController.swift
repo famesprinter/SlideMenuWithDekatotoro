f93f40f64a77ebbc2aea56cf4cf9f6225dd0bdfc
Kittitat Rodphotong <kittitat_rpt@hotmail.com>
2017-04-20 21:01:42 +0700

    Initial Commit

---
+++ SlideMenuWithDekatotoro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlideMenuWithDekatotoro
+//
+//  Created by Kittitat Rodphotong on 4/20/2560 BE.
+//  Copyright © 2560 Kittitat Rodphotong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
